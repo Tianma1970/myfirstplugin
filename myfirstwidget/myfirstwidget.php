9135d359f3f6139a343a15fc795d05c968002888
Tillmann Weimer <tillmann1970@gmail.com>
2019-05-20 20:47:35 +0200

    flyttat innehåll till ny fil för ändringar

--- myfirstwidget/myfirstwidget.php
+++ myfirstwidget/myfirstwidget.php
@@ -1,71 +1,18 @@
 <?php
-
 /**
- * Plugin Name:     My first Widget
- * Plugin URI:      https://tilmann1970.se/naturupplevelser
- * Description:     This is my first Widget
- * Version:         0.1
- * Author:          Tillmann Weimer
- * License:         WTFPL
- * License URI:     http://www.wtfpl.net/
- * Text Domain:     myFirstWidget
- * Domain Path:     languages/
+ * Plugin Name: My First Widget
+ * Plugin URI:  https://thehiveresistance.com/myfirstwidget
+ * Description: This plugin adds my first widget.
+ * Version:     0.1
+ * Author:      Johan Nordström
+ * Author URI:  https://thehiveresistance.com
+ * License:     WTFPL
+ * License URI: http://www.wtfpl.net/
+ * Text Domain: myfirstwidget
+ * Domain Path: /languages
  */
-
-require("class.MyFirstwidget.php");
-
- function mfw_widgets_init() {
-     register_widget('MyFirstWidget');
- }
- add_action('widgets_init', 'mfw_widgets_init');
- 
- function mfw_shortcode($atts=[]){ 
-
-     $mlp_atts = shortcode_atts([
-         //det samlade antal posts
-         'value' => 4,
-     ], $atts, $tag);
-     
-   
-       
-
-         $posts = new WP_Query([
-             'posts_per_page' => $mlp_atts['value'],
-         ]);
-
-         $output = "<h2>Latest Posts</h2>";
-        if($posts->have_posts()) {
-            $output .= "<ul>";
-            while($posts->have_posts()) {
-                $posts->the_post();
-
-            
-            $output .= "<li>";
-            $output .= "<a href = '" .get_the_permalink() . "'> ";
-            $output .=get_the_title();
-            $output .="<br>";
-            $output .=" Kategorier ";
-            $output .=get_the_category_list();
-            $output .=" by ";
-            $output .=get_the_author();
-            $output .=" at ";
-            $output .=get_the_date('Y-m-d');
-            $output .= "</a>";
-            $output .= "</li>";
-            }
-
-            mfw_reset_postdata();
-        $output .= "</ul>";
-
-        } else {
-            $output .="No latest posts available.";
-        }
-     return $output;
- }
-
- function mfw_init(){
-     add_shortcode('latest-posts', 'mfw_shortcode');
- }
-        
-    
- 
+require("class.MyFirstWidget.php");
+function mfw_widgets_init() {
+	register_widget('MyFirstWidget');
+}
+add_action('widgets_init', 'mfw_widgets_init');
