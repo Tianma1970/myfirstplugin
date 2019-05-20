a5fb430ea7f1484570001a81b8eb5e1f01fe1e62
Tillmann Weimer <tillmann1970@gmail.com>
2019-05-20 20:39:09 +0200

    små fixes

--- wcms18-latestposts/wcms18-latestposts.php
+++ wcms18-latestposts/wcms18-latestposts.php
@@ -11,44 +11,38 @@
  * Domain Path:     languages/
  */
 
- function wlp_shortcode($atts=[]){ 
-     $mlp_atts = shortcode_atts([
-         //det samlade antal posts
-         'value' => 4,
-     ], $atts, $tag);
+ function wlp_shortcode($atts = [], $content = null, $tag = '' ){ 
+     $default_atts = [
+         'posts' => 3,
+         'title'=> __('Latest Posts', 'wcms18-latestposts')
+     ];
+
+     $atts = shortcode_atts($default_atts, $user_atts, $tag);
+
+     $posts = new WP_Query ([
+         'post_per_page' => $atts['posts']
+     ]);
      
-   
-       
-
-         $posts = new WP_Query([
-             'posts_per_page' => $mlp_atts['value'],
-         ]);
-        
-
-
          //var_dump($atts)
 
-         
-     
-
-     $output = "<h2>Latest Posts</h2>";
+     $output = "<h2>". esc_html($atts['title']) . "</h2>";
         if($posts->have_posts()) {
             $output .= "<ul>";
             while($posts->have_posts()) {
                 $posts->the_post();
-
-            
             $output .= "<li>";
-            $output .= "<a href = '" .get_the_permalink() . "'> ";
+            $output .= "<a href = '" . get_the_permalink() . "'> ";
             $output .=get_the_title();
-            $output .="<br>";
-            $output .=" Kategorier ";
+            $output .= "</a>";
+
+            $output .="<small>";
+            $output .=" in ";
             $output .=get_the_category_list();
             $output .=" by ";
             $output .=get_the_author();
-            $output .=" at ";
-            $output .=get_the_date('Y-m-d');
-            $output .= "</a>";
+            $output .=" ";
+            $output .=human_time_diff(get_the_time('U')) . 'ago';
+            $output .= "</small>";
             $output .= "</li>";
         }
         wp_reset_postdata();
